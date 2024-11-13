3c8567991ecdf7b2b9b5a35f6094fc6bcd9a8f48
tradvara <traduxionesvaratas@gmail.com>
2024-11-13 14:21:47 +0000

    commit

--- start.sh
+++ start.sh
@@ -11,10 +11,12 @@ java -version
 
 # Descargar e instalar ngrok
 echo "Descargando e instalando ngrok..."
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip -O ngrok.zip
-unzip ngrok.zip
-sudo mv ngrok /usr/local/bin/ngrok
-rm ngrok.zip
+curl -sSL https://ngrok-agent.s3.amazonaws.com/ngrok.asc \
+  | sudo tee /etc/apt/trusted.gpg.d/ngrok.asc >/dev/null \
+  && echo "deb https://ngrok-agent.s3.amazonaws.com buster main" \
+  | sudo tee /etc/apt/sources.list.d/ngrok.list \
+  && sudo apt update \
+  && sudo apt install ngrok
 
 # Verificar la instalación de ngrok
 echo "Verificando la instalación de ngrok..."
@@ -24,27 +26,35 @@ ngrok version
 echo "Descargando PaperMC..."
 wget https://papermc.io/api/v2/projects/paper/versions/1.21.1/builds/131/downloads/paper-1.21.1-131.jar -O paper-1.21.1-131.jar
 
-# Crear el script para iniciar el servidor
+# Crear el script para iniciar el servidor.
 echo "Creando el script para iniciar el servidor de Minecraft..."
-cat > start_server.sh <<EOL
+cat > r_start_server.sh <<EOL
 #!/bin/bash
 # Iniciar el servidor de Minecraft con 1GB de RAM mínima y 2GB de RAM máxima
 java -Xms1G -Xmx2G -jar paper-1.21.1-131.jar nogui
 EOL
 
-# Crear el script para iniciar el servidor
+# Crear el script para iniciar ngrok.
 echo "Creando el script para iniciar el servidor de Minecraft..."
-cat > ngrok.sh <<EOL
+cat > r_ngrok.sh <<EOL
 #!/bin/bash
 # Iniciar el servidor de Minecraft con 1GB de RAM mínima y 2GB de RAM máxima
 ngrok authtoken 2om1MM3EqD8R48t9OS4WyfbwQ1Z_VNTxSnT76VSZB8ExKMUw
-ngrok tcp 25565
+ngrok http 25565
+EOL
+
+# Crear el script para eliminar todo.
+echo "Creando el script para iniciar el servidor de Minecraft..."
+cat > r_restart.sh <<EOL
+#!/bin/bash
+# Iniciar el servidor de Minecraft con 1GB de RAM mínima y 2GB de RAM máxima
+rm start_server.sh
+rm ngrok.sh
 EOL
 
 
-# Dar permisos de ejecución al script
-chmod +x start_server.sh
-chmod +x ngrok.sh
+# Dar permisos de ejecución a los scripts.
+chmod +x r_*
 
 # Instrucciones para ejecutar ngrok
 echo "Para exponer el servidor a través de ngrok, ejecuta el siguiente comando en otra terminal:"
